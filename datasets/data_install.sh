1cb5b5a1a5dd507ba1ebe0913981ad03e4ab9eef
ehpotsirhc <chris@tophelabs.com>
2023-10-26 01:33:12 -0400

    changed 'mv' to 'cp'

--- datasets/data_install.sh
+++ datasets/data_install.sh
@@ -2,23 +2,23 @@
 
 printf "----------------------------------------------------------------------"
 printf "=== Extracting LOTClass Data... ===\n"
-mv downloaded/LOTClass/agnews/* ../classifiers/LOTClass/datasets/agnews/
-mv downloaded/LOTClass/amazon/* ../classifiers/LOTClass/datasets/amazon/
-mv downloaded/LOTClass/dbpedia/* ../classifiers/LOTClass/datasets/dbpedia/
-mv downloaded/LOTClass/imdb/* ../classifiers/LOTClass/datasets/imdb/
+cp downloaded/LOTClass/agnews/* ../classifiers/LOTClass/datasets/agnews/
+cp downloaded/LOTClass/amazon/* ../classifiers/LOTClass/datasets/amazon/
+cp downloaded/LOTClass/dbpedia/* ../classifiers/LOTClass/datasets/dbpedia/
+cp downloaded/LOTClass/imdb/* ../classifiers/LOTClass/datasets/imdb/
 
 printf "----------------------------------------------------------------------"
 printf "=== Extracting WeSTClass Data... ===\n"
-mv downloaded/WeSTClass/agnews/ ../classifiers/WeSTClass/
-mv downloaded/WeSTClass/yelp/ ../classifiers/WeSTClass/
+cp downloaded/WeSTClass/agnews/ ../classifiers/WeSTClass/
+cp downloaded/WeSTClass/yelp/ ../classifiers/WeSTClass/
 
 printf "----------------------------------------------------------------------"
 printf "=== Extracting XClass Data... ===\n"
-mv downloaded/XClass/20News/* ../classifiers/XClass/data/datasets/20News/
-mv downloaded/XClass/AGNews/* ../classifiers/XClass/data/datasets/AGNews/
-mv downloaded/XClass/DBpedia/* ../classifiers/XClass/data/datasets/DBpedia/
-mv downloaded/XClass/NYT-Locations/* ../classifiers/XClass/data/datasets/NYT-Locations/
-mv downloaded/XClass/NYT-Small/* ../classifiers/XClass/data/datasets/NYT-Small/
-mv downloaded/XClass/NYT-Topics/* ../classifiers/XClass/data/datasets/NYT-Topics/
-mv downloaded/XClass/Yelp/* ../classifiers/XClass/data/datasets/Yelp/
+cp downloaded/XClass/20News/* ../classifiers/XClass/data/datasets/20News/
+cp downloaded/XClass/AGNews/* ../classifiers/XClass/data/datasets/AGNews/
+cp downloaded/XClass/DBpedia/* ../classifiers/XClass/data/datasets/DBpedia/
+cp downloaded/XClass/NYT-Locations/* ../classifiers/XClass/data/datasets/NYT-Locations/
+cp downloaded/XClass/NYT-Small/* ../classifiers/XClass/data/datasets/NYT-Small/
+cp downloaded/XClass/NYT-Topics/* ../classifiers/XClass/data/datasets/NYT-Topics/
+cp downloaded/XClass/Yelp/* ../classifiers/XClass/data/datasets/Yelp/
 
