0176b77f0d95eb70ab1086a96953246d2a509a3f
ehpotsirhc <chris@tophelabs.com>
2023-10-28 15:56:48 -0400

    decreased the batch size from 32 to 16 to accomodate a GPU with less vRAM

--- classifiers/XClass/scripts/run_train_text_classifier.sh
+++ classifiers/XClass/scripts/run_train_text_classifier.sh
@@ -8,6 +8,27 @@ model_name_or_path=bert-base-cased
 # this is also defined in utils.py, make sure to change both when changing.
 output_dir=../models/${model_name_or_path}_${train_suffix}
 
+# the paper's version has been commented out in order to run it 
+# on Chris's GPU (see below)
+# CUDA_VISIBLE_DEVICES=$GPU python train_text_classifier.py \
+#   --model_name_or_path ${model_name_or_path} \
+#   --task_name ${dataset_name} \
+#   --train_suffix ${train_suffix} \
+#   --test_suffix "" \
+#   --output_dir ${output_dir} \
+#   --do_train \
+#   --do_eval \
+#   --evaluate_during_training \
+#   --learning_rate 5e-5 \
+#   --num_train_epochs 3.0 \
+#   --max_seq_length 512 \
+#   --per_gpu_train_batch_size 32 \
+#   --per_gpu_eval_batch_size 32 \
+#   --logging_steps 100000 \
+#   --save_steps -1 
+
+
+# batch size changed from 32 to 16 to accomodate a GPU with only 12GBs of vRAM
 CUDA_VISIBLE_DEVICES=$GPU python train_text_classifier.py \
   --model_name_or_path ${model_name_or_path} \
   --task_name ${dataset_name} \
@@ -20,7 +41,8 @@ CUDA_VISIBLE_DEVICES=$GPU python train_text_classifier.py \
   --learning_rate 5e-5 \
   --num_train_epochs 3.0 \
   --max_seq_length 512 \
-  --per_gpu_train_batch_size 32 \
-  --per_gpu_eval_batch_size 32 \
+  --per_gpu_train_batch_size 16 \
+  --per_gpu_eval_batch_size 16 \
   --logging_steps 100000 \
   --save_steps -1 
+
