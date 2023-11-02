c3e55e5338bfd843aa8aac1721f75ce92b5297ef
ehpotsirhc <chris@tophelabs.com>
2023-11-02 02:28:20 -0400

    added Precision/Recall/F1 to the output

--- classifiers/XClass/scripts/utils.py
+++ classifiers/XClass/scripts/utils.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 import itertools
 import operator
 import os
 
 import numpy as np
+import pandas as pd
 
 linewidth = 200
 np.set_printoptions(linewidth=linewidth)
@@ -11,7 +13,7 @@ np.set_printoptions(precision=3, suppress=True)
 from collections import Counter
 
 from scipy.spatial.distance import cdist
-from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
 from transformers import BertModel, BertTokenizer
 
 MODELS = {
@@ -51,21 +53,67 @@ def most_common(L):
     return c.most_common(1)[0][0]
 
 
+def error_analysis(labels_true, labels_pred, **kwargs):
+    fpath_test_text = Path(DATA_FOLDER_PATH)/'SBIC/dataset.txt'
+    labels = pd.read_csv(Path(DATA_FOLDER_PATH)/'SBIC/classes.txt', header=None)[0].to_list()
+    test_text = pd.read_csv(fpath_test_text, header=None)[0].to_list()
+    
+    n_err = kwargs['n_err'] if 'n_err' in kwargs else 5   # default to 5
+    indices_wrong = np.argwhere((labels_pred==labels_true) == False).flatten()
+    n_correct = (np.array(labels_pred)==np.array(labels_true)).sum()
+    n_total = len(labels_true)
+
+    if indices_wrong.size >= n_err:
+        idx_examples = np.random.choice(indices_wrong, size=n_err, replace=False).tolist()
+        labels_pred = np.array(labels_pred).take(idx_examples).tolist()
+        labels_true = np.array(labels_true).take(idx_examples).tolist()
+        examples = np.array(test_text).take(idx_examples).tolist()
+        combined = list(zip(idx_examples, labels_pred, labels_true, examples))
+        return [{'index':e[0], 'label_pred':(labels[e[1]], e[1]), 'label_true':(labels[e[2]], e[2]), 'text':e[3]} for e in combined]
+    else:
+        return 'There are less than %s incorrect examples.' % n_err
+
+
+
+
 def evaluate_predictions(true_class, predicted_class, output_to_console=True, return_tuple=False):
     confusion = confusion_matrix(true_class, predicted_class)
     if output_to_console:
         print("-" * 80 + "Evaluating" + "-" * 80)
         print(confusion)
+    n_correct = (np.array(true_class)==np.array(predicted_class)).sum()
+    accuracy = accuracy_score(true_class, predicted_class)
+    precision_macro = precision_score(true_class, predicted_class, average='macro')
+    precision_micro = precision_score(true_class, predicted_class, average='micro')
+    recall_macro = recall_score(true_class, predicted_class, average='macro')
+    recall_micro = recall_score(true_class, predicted_class, average='micro')
     f1_macro = f1_score(true_class, predicted_class, average='macro')
     f1_micro = f1_score(true_class, predicted_class, average='micro')
     if output_to_console:
+        print("accuracy: %s  (%s/%s)" % (accuracy, n_correct, len(true_class)))
+        print("precision macro: %s" % precision_macro)
+        print("precision micro: %s" % precision_micro)
+        print("recall macro: %s" % recall_macro)
+        print("recall micro: %s" % recall_micro)
         print("F1 macro: " + str(f1_macro))
         print("F1 micro: " + str(f1_micro))
+
+        errors = error_analysis(true_class, predicted_class)
+        print('\nExamples of Incorrect Predictions...')
+        [print('- '*25, '\n', e) for e in errors]
+        print()
+
     if return_tuple:
-        return confusion, f1_macro, f1_micro
+        return confusion, f1_macro, f1_micro, accuracy, precision_macro, precision_micro, recall_macro, recall_micro
+        
     else:
         return {
             "confusion": confusion.tolist(),
             "f1_macro": f1_macro,
-            "f1_micro": f1_micro
+            "f1_micro": f1_micro, 
+            "accuracy": accuracy, 
+            "precision_macro": precision_macro, 
+            "precision_micro": precision_micro, 
+            "recall_macro": recall_macro, 
+            "recall_micro": recall_micro
         }
