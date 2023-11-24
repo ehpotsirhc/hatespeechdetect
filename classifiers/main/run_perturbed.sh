ca830cac18c9a4eb3652760ccffb75cdd6010c2e
ehpotsirhc <chris@tophelabs.com>
2023-11-24 10:35:45 -0500

    initial commit of run_perturbed.sh (batch runner for perturbed data)

---
+++ classifiers/main/run_perturbed.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dpath=../../datasets/SBIC/perturbed/
+suffix=csv
+declare -a torun=(
+    SBIC_robustness_DIV_swapTarget
+    SBIC_robustness_INV_contract
+    SBIC_robustness_INV_expand
+    SBIC_robustness_INV_typo
+    SBIC_robustness_MFT
+    SBIC_robustness_MFT_exhaustive
+    )
+
+
+for fname in ${torun[@]}
+do
+    echo ========== running $fname... ==========
+    python xclass.py --dataset $dpath$fname.$suffix
+    cp logs/xclass_run.log logs/archived/xclass_$fname.log
+done
+
